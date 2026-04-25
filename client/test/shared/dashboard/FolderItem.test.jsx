7b11a1a17ab5ab615dd84f6907758c98e9523cba
Kaushik Dhameliya <kdhameliya94@gmail.com>
2026-04-25 22:19:34 +0530

    fix: format issue

--- client/test/shared/dashboard/FolderItem.test.jsx
+++ client/test/shared/dashboard/FolderItem.test.jsx
@@ -3,44 +3,42 @@ import { screen, render, fireEvent } from "@testing-library/react";
 import FolderItem from "@/shared/components/dashboard/FolderItem";
 
 jest.mock("lucide-react", () => ({
-    Folder: () => <div>FolderIcon</div>
+  Folder: () => <div>FolderIcon</div>,
 }));
 
 describe("FolderItem", () => {
-    const folder = {
-        name: "test",
-        id: "10001",
-        docType: "folder",
-        sharedWith: [],
+  const folder = {
+    name: "test",
+    id: "10001",
+    docType: "folder",
+    sharedWith: [],
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("renders the folder name", () => {
+    const { getByText } = render(<FolderItem folder={folder} />);
+    expect(getByText(folder.name)).toBeInTheDocument();
+  });
+
+  it("renders the folder icon", () => {
+    const { getByText } = render(<FolderItem folder={folder} />);
+    expect(getByText("FolderIcon")).toBeInTheDocument();
+  });
+
+  it("calls onContextMenu on right click", async () => {
+    const onContextMenu = jest.fn();
+    const props = {
+      folder,
+      onContextMenu,
+      onNavigate: jest.fn(),
+      isSelected: false,
     };
-
-    beforeEach(() => {
-        jest.clearAllMocks();
-    });
-
-    it("renders the folder name", () => {
-        const { getByText } = render(<FolderItem folder={folder} />);
-        expect(getByText(folder.name)).toBeInTheDocument();
-    });
-
-    it("renders the folder icon", () => {
-        const { getByText } = render(<FolderItem folder={folder} />);
-        expect(getByText("FolderIcon")).toBeInTheDocument();
-    });
-
-    it("calls onContextMenu on right click", async () => {
-        const onContextMenu = jest.fn();
-        const props = {
-            folder,
-            onContextMenu,
-            onNavigate: jest.fn(),
-            isSelected: false,
-        }
-        render(
-            <FolderItem data-testid="folder-item" {...props} />,
-        );
-        const folderItem = screen.getByTestId("folder-item");
-        fireEvent.contextMenu(folderItem);
-        expect(onContextMenu).toHaveBeenCalledWith(expect.anything(), folder, "folder");
-    });
+    render(<FolderItem data-testid="folder-item" {...props} />);
+    const folderItem = screen.getByTestId("folder-item");
+    fireEvent.contextMenu(folderItem);
+    expect(onContextMenu).toHaveBeenCalledWith(expect.anything(), folder, "folder");
+  });
 });
